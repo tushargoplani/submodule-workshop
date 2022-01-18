4577cf4acb6173759490afe61fa7fc78f6a90ba4
Tushar Goplani <88302195+tushargoplani@users.noreply.github.com>
2022-01-18 15:49:02 +0530

    Add files via upload

---
+++ helloworld.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+var arr =[
+    {name:"tushar",age:19},
+    {name:"hello",age:21},
+    {name:"world",age:17},
+    {name:"heyThere",age:15}
+]
+console.table(arr);
+
+let student1 = {name:"Tushar",age:19};
+let student2 = {name:"Sunny",age:22};
+let student3= {name:"Girish",age:20};
+console.table([student1,student2,student3]);
