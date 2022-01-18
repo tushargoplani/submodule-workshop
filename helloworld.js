cf0f30f620e3295897229b7169c76f374c3754ff
tushargoplani <tushar7140@gmail.com>
2022-01-18 16:03:13 +0530

    Some changes and new file added

--- helloworld.js
+++ helloworld.js
@@ -1,14 +1,4 @@
 console.log("hello world");
 
-var arr =[
-    {name:"tushar",age:19},
-    {name:"hello",age:21},
-    {name:"world",age:17},
-    {name:"heyThere",age:15}
-]
-console.table(arr);
-
-let student1 = {name:"Tushar",age:19};
-let student2 = {name:"Sunny",age:22};
-let student3= {name:"Girish",age:20};
-console.table([student1,student2,student3]);
+var arr = [543,1,54,7,3,65,2];
+console.log(arr.sort((a,b)=>{return a-b}));
